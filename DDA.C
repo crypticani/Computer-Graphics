877090615da87fbd2f14ef0266a3ac11205cf094
Aniket Kumar <44335648+Aniketkjnv@users.noreply.github.com>
2020-02-11 11:15:30 +0530

    Add files via upload

---
+++ DDA.C
@@ -0,0 +1,42 @@
+#include<graphics.h>
+#include<stdlib.h>
+
+void dda(int, int, int, int);
+main()
+{
+
+	float x0=10,x1=10,y0=20,y1=50;
+
+	dda(x0,x1,y0,y1);
+
+}
+void dda(x0,x1,y0,y1){
+	int gd=DETECT,gm;
+	float dx,dy,i,xinc,yinc,step;
+
+		dx=(x1-x0);
+
+		dy=(y1-y0);
+
+		if (abs(dx)>abs(dy)){
+			step=abs(dx);
+		}
+		else{
+			step=abs(dy);
+		}
+		xinc=dx/step;
+		yinc=dy/step;
+
+		initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+
+	for(i=0;i<step;i++){
+		putpixel(x0,y0,RED);
+		x0=x0+xinc;
+		y0=y0+yinc;
+	}
+
+
+	line(100,20,10,50);
+	getch();
+	closegraph();
+}
---
+++ LINEP.C
@@ -0,0 +1,58 @@
+#include<graphics.h>
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+	{
+		int gd=DETECT, gm;
+		float x,y,x1,y1,x2,y2,m,c,i;
+
+		printf("X1:");
+		scanf("%f",&x1);
+		printf("Y1:");
+		scanf("%f",&y1);
+		printf("X2:");
+		scanf("%f",&x2);
+		printf("Y2:");
+		scanf("%f",&y2);
+
+		m=(y2-y1)/(x2-x1);
+		c=y1-(m*x1);
+
+
+		initgraph(&gd,&gm,"c:\\tc\\bgi");
+
+		line(x1,y1,x2,y2);
+
+		if(x2<x1)
+		{
+		    x=x1;
+		    x1=x2;
+		    x2=x;
+		    y=y1;
+		    y1=y2;
+		    y2=y;
+
+		for(i=x1;i<=x2;i++)
+		{
+			putpixel(i,(i*m)+c,RED);
+		}
+		}
+
+
+		else
+		{
+
+		for(i=x1;i<=x2;i++)
+		{
+			putpixel(i,(i*m)+c,RED);
+		}
+		}
+		outtextxy(0,420,"Coordinates of the line are : (x1,y1),(x2,y2)");
+		outtextxy(0,430,"Slope of the line is m.");
+
+
+		getch();
+		closegraph();
+
+	}
